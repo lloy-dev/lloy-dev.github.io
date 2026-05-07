bd95821474f4f98c51118cc623be6691f5ad2b83
Lloyd <25301009+lloy-dev@users.noreply.github.com>
2026-05-07 19:02:53 +0800

    Move html and necessary files to public folder

--- README.md
+++ README.md
@@ -2,15 +2,6 @@
 
 Welcome to my personal website built purely with HTML, CSS and JavaScript.
 
-<table align="center">
-  <tr>
-    <td align="center">
-      <img src="./docs/images/personal_site_thumbnail.jpg" width="100%"/><br>
-      <sub><em>Figure 1: Personal Website</em></sub>
-    </td>
-  </tr>
-</table>
-
 ## Setup
 
-Simply open the [index.html](./index.html) file directly in a browser.
+Simply open the [index.html](./public/index.html) file directly in a browser.
--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,13 @@ navTemplate.innerHTML = `
   <li><a href="/">Home</a></li>
   <li><a href="/contact">Contact</a></li>
 </ul>
-<img class="nav-logo" alt="Logo" src="./assets/lloydborres_logo_dark.svg" />
+<img class="nav-logo" alt="Logo" src="./assets/logo_dark.svg" />
 <button class="nav-menu-button" onclick="toggleMenu()">
   <img alt="Menu" src="./assets/icons/menu.svg" />
 </button>
 <div class="nav-socials">
   <a
-    href="https://github.com/lloydborres"
+    href="https://github.com/lloy-dev"
     target="_blank"
     rel="noopener noreferrer"
     ><img alt="GitHub Profile" src="./assets/icons/github_mark.svg"
@@ -21,9 +21,9 @@ navTemplate.innerHTML = `
 const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
 <p class="footer-copyright">
-  Crafted by Lloyd John Borres using pure HTML, CSS and JavaScript -
+  Crafted by Lloyd using pure HTML, CSS and JavaScript -
   <a
-    href="https://github.com/lloydborres/lloydborres.github.io"
+    href="https://github.com/lloy-dev/lloy-dev.github.io"
     target="_blank"
     rel="noopener noreferrer"
     >View on GitHub</a
@@ -31,11 +31,11 @@ footerTemplate.innerHTML = `
   © 2025. All rights reserved.
 </p>
 <div class="footer-socials">
-  <a href="mailto:dev@lloydborres.com" target="_blank" rel="noopener noreferrer"
+  <a href="mailto:contact@lloy.dev" target="_blank" rel="noopener noreferrer"
     ><img alt="Email" src="./assets/icons/mail.svg"
   /></a>
   <a
-    href="https://github.com/lloydborres"
+    href="https://github.com/lloy-dev"
     target="_blank"
     rel="noopener noreferrer"
     ><img alt="GitHub Profile" src="./assets/icons/github_mark.svg"
